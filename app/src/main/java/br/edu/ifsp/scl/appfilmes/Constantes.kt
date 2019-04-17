5d52bc8e33e6aeb8512e3d56dee23e8352a49f88
pos-graduacao-ifsp-caio <morcelicaio2@gmail.com>
2019-04-17 02:02:16 -0300

    Commit inicial. Inserção da chave da API.

---
+++ app/src/main/java/br/edu/ifsp/scl/appfilmes/Constantes.kt
@@ -0,0 +1,11 @@
+package br.edu.ifsp.scl.appfilmes
+
+object Constantes {
+    // Constantes usadas para acesso ao WebService de filmes
+    // val URL_BASE = ""
+
+    val APP_KEY_FIELD = "app_key"
+    // Preeencher com a app_key
+    val APP_KEY_VALUE = "3120449c"
+
+}
